c21248fbf627b50d6e303e85bada1239f94e2e24
mcmcmax437 <mcmcmax437@gmail.com>
2023-10-15 22:50:34 +0300

    RingCollection try

---
+++ Lab1 - Ring collection/Program.cs
@@ -0,0 +1,36 @@
+﻿using Lab1___Ring_collection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        RingCollection<int> ring = new RingCollection<int>(5);
+
+        ring.Add(1);
+        ring.Add(2);
+        ring.Add(3);
+        ring.Add(5);
+        ring.Add(-2);
+        ring.Add(100);
+        ring.Add(0);
+
+        bool isRing = ring.IsRingCollection();
+        Console.WriteLine("Is it a ring collection? " + isRing);
+
+        /*
+        ring.Delete();
+        isRing = ring.IsRingCollection();
+        Console.WriteLine("Is it a ring collection after dequeue? " + isRing); 
+        ring.Add(4);
+        ring.Add(5);
+        isRing = ring.IsRingCollection();
+        Console.WriteLine("Is it a ring collection after filling the buffer? " + isRing); 
+        ring.Add(6);
+        isRing = ring.IsRingCollection();
+        Console.WriteLine("Is it a ring collection after overwriting? " + isRing); 
+       */
+    }
+}
---
+++ Lab1 - Ring collection/RingCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1___Ring_collection
+{
+    public class RingCollection<T>
+    {
+        private T[] buffer;
+        private int capacity;
+        private int count;
+        private int head;
+        private int tail;
+
+        public RingCollection(int capacity)
+        {
+            if (capacity <= 0)
+                throw new ArgumentException("Capacity must be greater than zero.");
+
+            this.capacity = capacity;
+            this.buffer = new T[capacity];
+            this.count = 0;
+            this.head = 0;
+            this.tail = 0;
+        }
+
+        public int Count => count;
+
+        public bool IsRingCollection()
+        {
+            return count <= capacity && head >= 0 && head < capacity && tail >= 0 && tail < capacity;
+        }
+
+        public void Add(T item)
+        {
+            if (count == capacity)
+            {
+                // Buffer повний, перезаписує ластовий елемент
+                head = (head + 1) % capacity;
+            }
+            buffer[tail] = item;
+            tail = (tail + 1) % capacity;
+            count++;
+        }
+
+        public T Delete()
+        {
+            if (count == 0)
+                throw new InvalidOperationException("The ring collection is empty.");
+
+            T item = buffer[head];
+            head = (head + 1) % capacity;
+            count--;
+            return item;
+        }
+    }
+}
+
